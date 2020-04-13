b70b865545b38d2f0950918f7a33a0ddb65a037b
Guti-Haz <30297859+Guti-Haz@users.noreply.github.com>
2020-04-14 01:22:22 +0800

    Add files via upload

--- app.R
+++ app.R
@@ -1,105 +1,141 @@
-pacman::p_load(shiny,magrittr,data.table,DT,rstudioapi,stringr)
+# for local deploy
+pacman::p_load(shiny,magrittr,data.table,DT,rstudioapi,stringr,lubridate,tibble,rhandsontable)
 setwd(dirname(getSourceEditorContext()$path))
 source("global.R")
+# # for shinyapps.io
+# library(shiny)
+# library(magrittr)
+# library(data.table)
+# library(DT)
+# library(stringr)
+# library(lubridate)
+# library(tibble)
+# library(rhandsontable)
+# source("global.R")
 
 ui=fluidPage(
+    h4("Dashboard - Div II"),br(),
     tabsetPanel(id="tabSet",
-                tabPanel("Main",
+                tabPanel("Task manager",br(),
+                         actionButton("add_button","",icon("plus"),style=css.button),br(),
                          DTOutput("table")
                 ),
-                tabPanel("Data input",
-                         uiOutput("inputList")
-                )
+                tabPanel("View"),
+                tabPanel("Download")
     )
 )
 
 server=function(input, output,session){
+    valToday=reactive({
+        invalidateLater(4.32e+7,session) #every 12 hours
+        today()
+    })
+    
+    observeEvent(input$add_button,{
+        showModal(
+            modalDialog(
+                fluidPage(
+                    tabsetPanel(id="tabSet2",
+                                tabPanel("Basic",br(),
+                                         selectInput("add_corr","Correspondence",c("",corr_list)),
+                                         selectInput("add_nature","Nature",""),
+                                         selectInput("add_counterparty","Counterparty",""),
+                                         textInput("add_refNum","Reference No.",value=genRandom()),
+                                         dateInput("add_date","Date",value=valToday()),
+                                         selectInput("add_analyst","Analyst",c("",analyst_list)),
+                                         selectInput("add_supervisor","Supervisor",c("",supervisor_list))
+                                ),
+                                tabPanel("Crime",br(),
+                                         selectInput("add_offence","Offence",c("",sort(unique(crime$offence)))),
+                                         textInput("add_offenceDesc","Offence description"),
+                                         selectizeInput("add_law-prov","Law and provision","",width='400px',multiple=T)
+                                ),
+                                tabPanel("Intel",br(),
+                                         #names/id/accounts
+                                )
+                    )
+                ),
+                actionButton("add_submit","",icon("check-circle"),style=css.button),
+                easyClose=T,footer=NULL
+            )
+        )
+    })
+    
     observe({
-        if(input$tabSet=="Data input"){
-            showModal(
-                modalDialog(
-                    selectInput("corr_type1","Counterparty",corr_type1_list,size=3,selectize=F),
-                    selectInput("corr_type2","Nature","",size=4,selectize=F),
-                    actionButton("corr_submit","Submit")
-                )
-            )   
+        req(input$add_corr)
+        if (input$add_corr=="Local"){
+            updateSelectInput(session,"add_nature",choices=nature_list[c(1,4)])
+            updateSelectInput(session,"add_counterparty",choices=lea_list)
+        }
+        if (input$add_corr=="ESW"){
+            updateSelectInput(session,"add_nature",choices=nature_list)
+            updateSelectInput(session,"add_counterparty",choices=country_list)
+        }
+        if (input$add_corr=="Special - MLA"){
+            updateSelectInput(session,"add_nature",choices=nature_list[1])
+            updateSelectInput(session,"add_counterparty",choices=country_list)
         }
     })
     
-    observeEvent(input$corr_type1,{
-        if (input$corr_type1=="Local LEA"){temp=corr_type2_list[c(1,4)]}
-        if (input$corr_type1=="Foreign FIU - ESW"){temp=corr_type2_list}
-        if (input$corr_type1=="AGC - MLA"){temp=corr_type2_list[1]}
-        updateSelectInput(session,"corr_type2",choices=temp)
-    })
-    
-    observeEvent(input$corr_submit,{
-        removeModal(session)
-        output$inputList=renderUI({
-            tagList(
-                selectInput("data_counterparty","Counterparty",""),
-                textInput("data_refNum","Reference No.",value=genRandom()),
-                dateInput("data_date","Date"),
-                actionButton("data_submit","Submit")
-            )
-        })
-        if(input$corr_type1=="Local LEA"){temp=data_counterparty_list_localLEA}
-        if(input$corr_type1=="Foreign FIU - ESW"){temp=data_counterparty_list_ffiu}
-        if(input$corr_type1=="AGC - MLA"){temp=data_counterparty_list_agc}
-        updateSelectInput(session,"data_counterparty",choices=temp)
+    observe({
+        req(input$add_offence)
+        updateSelectizeInput(session,"add_law-prov",
+                             choices=crime[offence==input$add_offence,sort(law_provision)],server=T)
     })
     
     dt=reactiveVal(data.table())
     
-    observeEvent(input$data_submit,{
-        output$inputList=renderUI({""})
+    observeEvent(input$add_submit,{
         dt_new=data.table(
-            Type=input$corr_type1,
-            Nature=input$corr_type2,
-            Counterparty=input$data_counterparty,
-            RefNum1=input$data_refNum,
-            Date1=input$data_date) %>% 
-            .[,Assign:=makeButton("Assign",RefNum1)] %>%  #manage button
-            .[Nature=="Incoming Request",Response:=makeButton("Response",RefNum1)] #manage button
+            Correspondence=input$add_corr,
+            Nature=input$add_nature,
+            Counterparty=input$add_counterparty,
+            RefNum1=input$add_refNum,
+            Date1=as.character(input$add_date),
+            RefNum2="",
+            Date2="",
+            Analyst=input$add_analyst) %>% 
+            .[Analyst=="",Update_Assign:=makeButton("assign",RefNum1)] %>%
+            .[Nature=="Incoming Request",Update_Response:=makeButton("response",RefNum1)]
         dt_merged=rbindlist(list(dt(),dt_new),fill=T)
         dt(dt_merged)
-        updateTabsetPanel(session,"tabSet",selected="Main")
+        removeModal(session)
     })
     
-    observeEvent(input$Response_button,{
+    observeEvent(input$response_button,{
         showModal(
             modalDialog(
                 textInput("response_refNum","Reference No.",value=genRandom()),
-                dateInput("response_date","Date"),
-                actionButton("response_submit","Submit")
+                dateInput("response_date","Date",value=valToday()),
+                actionButton("response_submit","",icon("check-circle"),style=css.button),
+                easyClose=T,footer=NULL
             )
         )  
     })
     
     observeEvent(input$response_submit,{
-        selectedRefNum1=str_replace(input$Response_button,"Response_","")
-        dt_new=dt() %>% 
-            .[RefNum1==selectedRefNum1,`:=`(RefNum2=input$response_refNum,
-                                            Date2=input$response_date,
-                                            Response=NA)] #manage button
+        row=str_replace(input$response_button,"response_","")
+        dt_new=dt()[RefNum1==row,`:=`(RefNum2=input$response_refNum,
+                                      Date2=as.character(input$response_date),
+                                      Update_Response=NA)]
         dt(dt_new)
         removeModal(session)
     })
     
-    observeEvent(input$Assign_button,{
+    observeEvent(input$assign_button,{
         showModal(
             modalDialog(
-                selectInput("assign_analyst","Analyst",assign_list),
-                actionButton("assign_submit","Submit")
+                selectInput("assign_analyst","Analyst",analyst_list),
+                actionButton("assign_submit","",icon("check-circle"),style=css.button),
+                easyClose=T,footer=NULL
             )
         )  
     })
     
     observeEvent(input$assign_submit,{
-        selectedRefNum1=str_replace(input$Assign_button,"Assign_","")
-        dt_new=dt() %>% 
-            .[RefNum1==selectedRefNum1,`:=`(Assignee=input$assign_analyst,
-                                            Assign=NA)] #manage button
+        row=str_replace(input$assign_button,"assign_","")
+        dt_new=dt()[RefNum1==row,`:=`(Analyst=input$assign_analyst,
+                                      Update_Assign=NA)]
         dt(dt_new)
         removeModal(session)
     })
@@ -107,14 +143,26 @@ server=function(input, output,session){
     tableDT=eventReactive({
         input$assign_submit
         input$response_submit
-        input$corr_submit},{
+        input$add_submit},{
             dt()
         })
     
     output$table=renderDT(
-        tableDT(),
-        escape=F
+        tableDT() %>%
+            DT::datatable(
+                extensions="FixedHeader",
+                rownames=F,
+                escape=F,
+                options=list(
+                    processing=T,
+                    autowidth=T,
+                    fixedHeader=T,
+                    pageLength=1e4,
+                    dom="t"
+                )
+            )
     )
 }
 
 shinyApp(ui=ui,server=server)
+#rsconnect::deployApp(dirname(rstudioapi::getSourceEditorContext()$path),launch.browser=F,forceUpdate=T)
--- global.R
+++ global.R
@@ -1,35 +1,49 @@
-corr_type1_list=c("Local LEA",
-                  "Foreign FIU - ESW",
-                  "AGC - MLA") %>% 
+corr_list=c("Local",
+            "ESW",
+            "Special - MLA")
+nature_list=c("Incoming Request",
+              "Outgoing Request",
+              "Incoming Spontaneous",
+              "Outgoing Spontaneous") %>% 
   sort
-corr_type2_list=c("Incoming Request",
-                  "Outgoing Request",
-                  "Incoming Spontaneous",
-                  "Outgoing Spontaneous") %>% 
+lea_list=c("IMI",
+           "MACC",
+           "RMC",
+           "RMP - E5",
+           "RMP - E8") %>% 
   sort
-data_counterparty_list_localLEA=c("IMI",
-                                  "MACC",
-                                  "RMC",
-                                  "RMP - E5",
-                                  "RMP - E8") %>% 
+country_list=c("Nigeria",
+               "Ghana",
+               "Senegal",
+               "South Africa",
+               "Bhutan") %>% 
   sort
-data_counterparty_list_ffiu=c("STRO",
-                              "FinCEN",
-                              "AUSTRAC",
-                              "AMLO",
-                              "JAFIC") %>% 
-  sort
-data_counterparty_list_agc=c("Nigeria",
-                             "Ghana",
-                             "Senegal",
-                             "South Africa",
-                             "Bhutan") %>% 
-  sort
-assign_list=c("MFF",
+analyst_list=c("MFF",
               "NHZ",
               "THL",
               "AFA",
-              "MNAK")
+              "MNAK") %>% 
+  sort
+supervisor_list=c("ALBA",
+                  "LHH",
+                  "LWL") %>% 
+  sort
+crime=tribble(
+  ~law_provision, ~offence,
+  "Penal Code 1999: 11 - Attempted murder", "Murder",
+  "Income Tax Act 2011: 201 - Willfull evasion", "Tax evasion",
+  "Income Tax Act 2011: 202 - Hiding info", "Tax evasion",
+  "Penal Code 1999: 17 - Initiating bribery act", "Corruption",
+  "Corruption Act 2001: 4 - Accepting gratification", "Corruption",
+  "Corruption Act 2001: 5 - Giving gratification", "Corruption"
+) %>% 
+  setDT
+offence_list=c("Fraud",
+               "Murder",
+               "Tax evasion",
+               "Terrorism financing",
+               "Proliferation financing") %>% 
+  sort
 genRandom=function(){
   runif(1,1e7,1e8) %>% 
     round(0) %>% 
@@ -38,6 +52,7 @@ genRandom=function(){
 makeButton=function(type,ref){
   js=paste0('Shiny.setInputValue(\"',type,'_button\",this.id,{priority:"event"})')
   paste0(type,"_",ref) %>% 
-    actionButton(.,label="Update",onclick=js,style="display:inline-block") %>% 
+    actionButton(.,label="",icon("pen"),style=css.button,onclick=js) %>% 
     as.character
 }
+css.button="color: #fff; background-color: #337ab7; border-color: #2e6da4"
